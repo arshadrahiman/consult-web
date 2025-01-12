f4a87df6a86f3fe71546705ceec6dea2e2364c45
Arshadul Rahman <58355115+arshadrahiman@users.noreply.github.com>
2025-01-12 20:30:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-$(document).ready(function () {
-    let currentSlide = 0;
-    const slides = $(".testimonial-slide");
-    const totalSlides = slides.length;
-
-    function showSlide(index) {
-        slides.removeClass("active");
-        $(slides[index]).addClass("active");
-    }
-
-    function nextSlide() {
-        currentSlide = (currentSlide + 1) % totalSlides;
-        showSlide(currentSlide);
-    }
-
-    function prevSlide() {
-        currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
-        showSlide(currentSlide);
-    }
-
-    setInterval(nextSlide, 5000); // Change slide every 5 seconds
-    showSlide(currentSlide);
+$(document).ready(function(){
+    $('.testimonial-slider').slick({
+        autoplay: true,                // Automatically start the slider
+        autoplaySpeed: 5000,           // Change slide every 5 seconds
+        dots: true,                    // Enable navigation dots
+        arrows: true,                  // Enable previous/next arrows
+        fade: true,                    // Enable fade effect for a smooth transition
+        speed: 1000,                   // Slide transition speed
+        pauseOnHover: true,            // Pause the slider when hovered over
+        responsive: [
+            {
+                breakpoint: 768,      // For small screens, show only 1 slide at a time
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 });
